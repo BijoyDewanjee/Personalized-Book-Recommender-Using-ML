e2cd30452c411df82e8833edf4ba15f94c1258dc
BijoyDewanjee <bijoydewanjee103@gmail.com>
2026-04-10 12:48:25 +0600

    logger,exception and utils added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup (
+        name='books_recommender',
+        version ="0.0.1",
+        packages=find_packages(),
+        author="Bijoy Dewanjee",
+        author_email="bijoydewanjee103@gmail.com",
+        install_requires = []
+)
+
+
+
